aed4a0eaa5895872f5152e2cb0fe658e3fb3db14
Rashi bajpai <168172417+rashibajpai12@users.noreply.github.com>
2026-06-19 13:22:27 +0530

    Update recommendation_agent.py

--- agents/recommendation_agent.py
+++ agents/recommendation_agent.py
@@ -1,1 +1,31 @@
+import google.generativeai as genai
 
+def generate_recommendations(df, api_key):
+
+    genai.configure(api_key=api_key)
+
+    model = genai.GenerativeModel(
+        "gemini-2.5-flash"
+    )
+
+    prompt = f"""
+You are a carbon intelligence agent.
+
+Dataset:
+{df.to_string()}
+
+Identify:
+
+1. Largest carbon hotspot
+2. Carbon leakage source
+3. Reduction opportunities
+4. ESG action plan
+
+Keep concise.
+"""
+
+    response = model.generate_content(
+        prompt
+    )
+
+    return response.text
